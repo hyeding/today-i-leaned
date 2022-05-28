92c80aca8c89bda15d56c3a60536c639285e6582
hyeding <65836744+hyeding@users.noreply.github.com>
2022-05-29 01:26:28 +0900

    1001번 문제 풀이

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,17 +1,12 @@
-package baekjoon;
-import java.util.*; //Scanner 클래스를 위해 자바 모든 클래스 사용 가능하도록 import 함
-                    //또는 import java.util.Scanner;로 Scanner 클래스만 import 가능함
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);  //사용자로부터 값을 입력받기 위한 Scanner 클래스 생성
-        // 빈공간에 sc를 입력하면 import에 Scanner 클래스 자동생성됨
-
-
-        int A, B; //정수형태의 A와 B 선언
-
-        A = sc.nextInt(); //sc.nextInt() : 자료형에 따라 입력받음. 다음 토큰을 int 타입으로 리턴
-        B = sc.nextInt(); //Scanner클래스의 메소드. https://cocoon1787.tistory.com/38 참고
-        System.out.println(A+B);
+        //정수표현이 가능한 Scanner 클래스의 Scanner 객체 생성. Scanner클래스에는 정수,실수,문자열을 읽어올 수 있음
+        Scanner sc = new Scanner(System.in);
+        //Scanner클래스의 메소드 중 .next를 사용할 수 있음. 정수형태로 나타내야하므로 Int 사용
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A - B);
     }
 }

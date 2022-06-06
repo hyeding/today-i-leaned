b584d7cf2375a950ebc38fb81367f312d478166d
hyeding <65836744+hyeding@users.noreply.github.com>
2022-06-06 19:24:21 +0900

    초급-정수 내림차순으로 배치하기

--- 프로그래머스/Solution.java
+++ 프로그래머스/Solution.java
@@ -1,11 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public String solution(int num) {
-        if(num % 2 == 0){ //num을 2로 나눴을때 나머지가 0 >>짝수
-            return "Even"; // Even 리턴
-        } else{  //그렇지 않으면>>홀수
-            return "Odd"; //Odd 리턴
-        }
-        }
+    public long solution(long n) {
+        String[] list = String.valueOf(n).split(""); //n을 String 타입으로 형변환하고 split으로 문자구분
+        Arrays.sort(list); //list 배열을 오름차순으로 정렬
+
+        StringBuilder sb = new StringBuilder(); //가변성을 가지는 StringBuilder 클래스 사용하여 객체 생성
+        for(String aList : list) sb.append(aList); //StringBuilder의 API로 .append 사용하여 문자열 변경
+
+        return Long.parseLong(sb.reverse().toString()); //reverse로 오름차순 문자열을 뒤집어줌.
+        // .parseLong을 이용하여 문자열을 숫자 Long으로 변환
+    }
 }
